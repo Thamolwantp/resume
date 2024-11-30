18722baaaf11031730408a3ed8af9952805612fd
Thamolwantp <151597668+Chanon0000@users.noreply.github.com>
2024-11-30 16:51:54 +0700

    firstcommit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,115 +1,112 @@
-import Image from "next/image";
-import localFont from "next/font/local";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <h1>RESUME</h1>
+        <nav className={styles.nav}>
+          <a href="#about">ABOUT ME</a>
+          <a href="#skills">MY SKILLS</a>
+          <a href="#projects">PROJECTS</a>
+        </nav>
+      </header>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+      <section className={styles.section} id="about">
+        <div className={styles.card}>
+          <div className={styles.picture}>
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src="/image/My.jpg"   // Path ที่ถูกต้อง
+              alt="Description of Image"     // คำอธิบายของรูปภาพ
+              width={400}                    // ความกว้าง
+              height={400}                  // ความสูง
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
+          <div>
+            <h2>HELLO, I’M THAMONWAN </h2>
+            <p className={styles.text}>
+            I’m truly excited and honored that you have taken an interest in my resume. It means a lot to me, and I am hopeful that in the future, there will be an opportunity for us to collaborate and work together. I believe that with our combined skills and expertise, we could accomplish great things and contribute to each other’s success.
+            </p>
+            <h3>information</h3>
+            <ul className={styles.text}>
+              <li>Name Thamonwan Tonphong</li>
+              <li>Age 20</li>
+              <li>Bachelor degree</li>
+              <li>Software engineering</li>
+              <li>Phayao of University</li>
+            </ul>
+            <h3 className={styles.text}>Contact</h3>
+            <div className={styles.contact}>
+              <button>Tel 064-360-6328</button>
+              <button>Email Thamolwan.yong@gmail.com</button>
+              <button>Line</button>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      </section>
+
+      <section className={styles.section} id="skills">
+        <h2>MY SKILLS</h2>
+        <div className={styles.skills}>
+          <div>
+            <h3>PROGRAMMING</h3>
+            <ul>
+              <li>UX/UI DESIGN</li>
+              <li>DATABASE</li>
+              <li>HTML/CSS</li>
+            </ul>
+            <h3>TOOLS</h3>
+            <ul>
+              <li>Visual Studio Code</li>
+              <li>XAMPP</li>
+              <li>Figma</li>
+            </ul>
+            <h3>LANGUAGE</h3>
+            <p>ENGLISH (medium)</p><br/>
+          </div>
+          <div className={styles.circlePicture}>
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+              src="/image/chart.png"   // Path ที่ถูกต้อง
+              alt="Description of Image"     // คำอธิบายของรูปภาพ
+              width={400}                    // ความกว้าง
+              height={400}                  // ความสูง
+            />
+          </div>
+        </div>
+      </section>
+
+      <section className={styles.section} id="projects">
+        <h2>PROJECT</h2>
+        <div className={styles.project}>
+          <div className={styles.projectImage}>
           <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+              src="/image/fig1.png"   // Path ที่ถูกต้อง
+              alt="Description of Image"     // คำอธิบายของรูปภาพ
+              width={950}                    // ความกว้าง
+              height={450}                  // ความสูง
+            />
+          </div>
+          <div className={styles.textproj}>
+            <h3>VESTRA CINEPLAX</h3>
+            <p>text text An elegantly designed and intuitive movie ticket booking website that provides a seamless user experience</p>
+            <button>veiw</button>
+          </div><br/><br/><br/>
+          <div className={styles.projectImage} >
           <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+              src="/image/fig2.png"   // Path ที่ถูกต้อง
+              alt="Description of Image"     // คำอธิบายของรูปภาพ
+              width={950}                    // ความกว้าง
+              height={450}                  // ความสูง
+            />
+          </div>
+          <div className={styles.textproj}>
+            <h3>FOOD HOUSE</h3>
+            <p>A charming and visually appealing recipe website designed with a cute style and vibrant colors that are sure to stimulate your appetite and make you crave delicious food</p>
+            <button>veiw</button>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
